9c95dd5134538d29758d59af6851f032bd23b0f7
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-19 09:31:42 -0400

    fetch users list from api

--- client/src/User.jsx
+++ client/src/User.jsx
@@ -1,7 +1,22 @@
 import UserItem from "./UserItem";
+import axios from 'axios'
+import { useEffect, useState } from "react";
 
 export default function User() {
+  const [ users, setUsers ] = useState({})
+  
+  useEffect(() => {
+    const fetchUsers = async () => {
+      axios.get('/users').then((data) => {
+        setUsers(data.data)
+        console.log(data.data)
+      })
+    }
+    fetchUsers()
+  }, [])
   return(
-    <article>List</article>
+    <article>
+      <h2>Random Users</h2>
+    </article>
   )
 }
