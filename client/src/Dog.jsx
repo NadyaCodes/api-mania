9c5fd22ac46355a6ad98219bee3e4b9f4b70f4fc
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-19 09:55:32 -0400

    fetch and display dog image

--- client/src/Dog.jsx
+++ client/src/Dog.jsx
@@ -1,5 +1,18 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 export default function Dog() {
+  const [dog, setDog] = useState('')
+  
+  useEffect(() => {
+    const fetchDog = async () => {
+      axios.get("https://dog.ceo/api/breeds/image/random").then((data) =>
+        setDog(data.data.message)
+      )
+    }
+    fetchDog()
+  }, [])
   return(
-    <article>Dog</article>
+    <article><img src={dog} /></article>
   )
 }
